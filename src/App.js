2f6777b49302530bab57749bd3b0689056ea8caa
etriv <itai.riv@gmail.com>
2020-03-17 13:19:43 +0100

    Add page navigation to gallery

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import { fetchGifs } from './modules/giphy-manager';
 
 function App() {
   const [gifs, setGifs] = useState([]);
-  const [totalCount, setTotalCount] = useState(0);
+  const [totalGifsCount, setTotalGifsCount] = useState(0);
   const [gifsOffset, setGifsOffset] = useState(0);
-  const gifsLimit = 12;
+  const [curPage, setCurPage] = useState(0);
+
+  const gifsPageLimit = 12;   // Number of gifs to display in each page
+  const pageAmount = 10;      // The number of pages to fill with gifs
 
   const [searchText, setSearchText] = useState('dogs');
 
-  useEffect(onSearchClick, [])
+  useEffect(onSearchClick, []);
+
+  useEffect(() => {
+    // console.log('Number of pages:', Math.ceil(totalGifsCount / gifsPageLimit));
+  });
 
   function onSearchTextChange(e) {
     setSearchText(e.target.value);
@@ -23,14 +30,20 @@ function App() {
   }
 
   function onSearchClick() {
-    fetchGifs(searchText, gifsOffset, gifsLimit)
+    fetchGifs(searchText, gifsOffset, gifsPageLimit * pageAmount)
       .then(data => {
         console.log('Fetched gifs:', data);
-        setTotalCount(data.totalCount);
+        setCurPage(1);
+        setTotalGifsCount(data.totalCount);
         setGifs(data.newGifs);
+
       }).catch(err => {
         console.log('Error while fetching gifs:', err);
       });
+  }
+
+  function onPageChange(newPage) {
+    setCurPage(newPage);
   }
 
   return (
@@ -43,18 +56,45 @@ function App() {
         onKeyUp={onKeyUp} />
       <button className="search-button"
         onClick={onSearchClick}>
-        SEARCH</button>
+        🔍</button>
+      {curPage > 0 ?
+        <PageNav curPage={curPage}
+          totalPages={Math.ceil(totalGifsCount / gifsPageLimit)}
+          onPageChange={onPageChange} />
+        : null}
       <div className="gallery">
         {gifs.length > 0 ?
-          gifs.map((gif) =>
-            <div className="gif-container"
-              style={{
-                backgroundImage: `url(${gif.stillUrl})`
-              }}
-              key={gif.giphyId}></div>
-          )
+          gifs.slice((curPage-1) * gifsPageLimit, (curPage-1) * gifsPageLimit + gifsPageLimit)
+            .map((gif, index) =>
+              <div className="gif-container"
+                style={{
+                  backgroundImage: `url(${gif.stillUrl})`
+                }}
+                key={index}></div>
+            )
           : null}
       </div>
+      {curPage > 0 ?
+        <PageNav curPage={curPage}
+          totalPages={Math.ceil(totalGifsCount / gifsPageLimit)}
+          onPageChange={onPageChange} />
+        : null}
+    </div>
+  );
+}
+
+function PageNav({ curPage, totalPages, onPageChange }) {
+  return (
+    <div className="page-nav">
+      <button className="nav-btn"
+        disabled={curPage === 1}
+        onClick={() => onPageChange(curPage - 1)}>
+        Previous</button>
+      <div className="nav-numbers"><b>{curPage}</b> of {totalPages}</div>
+      <button className="nav-btn"
+        disabled={curPage === totalPages}
+        onClick={() => onPageChange(curPage + 1)}>
+        Next</button>
     </div>
   );
 }
